852710c5326d7c9d5631cc87459e1bcea6d8839c
mtired <mtired@example.com>
2026-01-03 15:19:55 +0900

    登録画面_バリデーション作成

--- src/app/Http/Controllers/RegisterController.php
+++ src/app/Http/Controllers/RegisterController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\RegisterRequest;
+use Laravel\Fortify\Contracts\CreatesNewUsers;
 
 class RegisterController extends Controller
 {
@@ -10,5 +12,13 @@ class RegisterController extends Controller
     public function index(Request $request)
     {
         return view('register');
+    }
+
+    public function store(RegisterRequest $request, CreateNewUsers $creator)
+    {
+        $input = $request->safe()->only(['name', 'email', 'password']);
+        $user = $creator->create($input);
+        //$user = $creator->create($request->validated());
+        return redirect('/register');
     }
 }
--- src/routes/web.php
+++ src/routes/web.php
@@ -4,3 +4,4 @@ use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [RegisterController::class, 'index']);
+Route::post('/register', [RegisterController::class, 'store']);
