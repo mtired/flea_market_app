cb590767f6ef90e7ae6f977f97fd079601accc3e
mtired <mtired@example.com>
2026-01-07 23:25:34 +0900

    ログイン認証修正

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -38,8 +38,12 @@ class User extends Authenticatable
      *
      * @var array<string, string>
      */
-    protected $casts = [
+    /*protected $casts = [
         'email_verified_at' => 'datetime',
+    ];*/
+
+    protected $casts = [
+    'profile_completed_at' => 'datetime',
     ];
 
     public function profile()
